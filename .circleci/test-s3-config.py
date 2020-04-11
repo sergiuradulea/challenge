8a0d2fe5395d2b45e9e5358e53418fbb6cade4cd
Sergiu Radulea <sergo.ciuca@gmail.com>
2020-04-11 17:02:25 +0100

    tests3config

---
+++ .circleci/test-s3-config.py
@@ -0,0 +1,15 @@
+import boto3
+import botocore
+import os
+
+BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+
+s3 = boto3.client('s3')
+
+#Check versioning of the bucket. 
+versioning = s3.get_bucket_versioning(
+    Bucket=BUCKET_NAME
+)
+
+if versioning["Status"] is not "Enabled":
+	raise ValueError(f'Versioning is disabled on bucket: {BUCKET_NAME}')
