9a23f339e88fa328dcefb776697bd5dec8383bad
Sergiu Radulea <sergo.ciuca@gmail.com>
2020-04-11 17:25:30 +0100

    test

--- .circleci/test-s3-config.py
+++ .circleci/test-s3-config.py
@@ -11,9 +11,18 @@ s3 = boto3.client('s3')
 versioning = s3.get_bucket_versioning(
     Bucket=BUCKET_NAME
 )
+
 print(versioning)
+
 try:
 	if versioning["Status"] == "Enabled":
 		print(f'Versioning is enabled on bucket: {BUCKET_NAME}')
 except:
 	raise ValueError(f'Versioning is disabled on bucket: {BUCKET_NAME}')
+
+
+hosting = s3.get_bucket_website(
+    Bucket=BUCKET_NAME
+)
+
+print(hosting0)
